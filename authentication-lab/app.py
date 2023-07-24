c76a958b148b6d9d81d9711657d97c64869b02ac
katiaam24-meet <139688886+katiaam24-meet@users.noreply.github.com>
2023-07-24 14:15:33 +0300

    d
    d

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -3,34 +3,43 @@ from flask import session as login_session
 import pyrebase
 
 
-conflig ={
-  apiKey: "AIzaSyBK97k9ZwZAaKhBjXrdLcHleTyqerq_EWc",
-  authDomain: "searswillwin.firebaseapp.com",
-  projectId: "searswillwin",
-  storageBucket: "searswillwin.appspot.com",
-  messagingSenderId: "493830207082",
-  appId: "1:493830207082:web:46e386368da2b1b47a7c13",
-  measurementId: "G-9FN4XLYV0L"
-  "databaseURL":""
+config ={
+  "apiKey": "AIzaSyBK97k9ZwZAaKhBjXrdLcHleTyqerq_EWc",
+  "authDomain": "searswillwin.firebaseapp.com",
+  "projectId": "searswillwin",
+  "storageBucket": "searswillwin.appspot.com",
+  "messagingSenderId": "493830207082",
+  "appId": "1:493830207082:web:46e386368da2b1b47a7c13",
+  "measurementId": "G-9FN4XLYV0L",
+  "databaseURL":"https://searswillwin-default-rtdb.firebaseio.com/"
   }
+
+firebase =pyrebase.initialize_app(config)
+auth= firebase.auth()
+db = firebase.database()
+
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
-@app.route('/signin', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def signin():
    error = ""
    if request.method == 'POST':
        email = request.form['email']
        password = request.form['password']
        try:
-            login_session['user'] = 
-auth.sign_in_with_email_and_password(email, password)
+           login_session['user'] =auth.sign_in_with_email_and_password(email, password)
            return redirect(url_for('add_tweet'))
        except:
            error = "Authentication failed"
-   return render_template("signin.html")
+           return render_template("signin.html")
+   else:
+        return render_template("signin.html")
+
+ 
+
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -40,8 +49,7 @@ def signup():
        email = request.form['email']
        password = request.form['password']
        try:
-            login_session['user'] = 
-auth.create_user_with_email_and_password(email, password)
+           login_session['user'] =auth.create_user_with_email_and_password(email, password)
            return redirect(url_for('add_tweet'))
        except:
            error = "Authentication failed"
