81c87212692261c67b70cf37773f91dff12b6fa1
katiaam24-meet <139688886+katiaam24-meet@users.noreply.github.com>
2023-07-25 13:53:55 +0300

    f
    f

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -38,27 +38,48 @@ def signin():
    else:
         return render_template("signin.html")
 
- 
 
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweets():
+    tweets = db.child("Tweets").get().val()
+    return render_template("all_tweets.html", tweets=tweets)
+
+
+    
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-   error = ""
-   if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-           login_session['user'] =auth.create_user_with_email_and_password(email, password)
-           return redirect(url_for('add_tweet'))
-       except:
-           error = "Authentication failed"
-   return render_template("signup.html")
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        username = request.form['username']
+        name = request.form['fullname']
+        bio= request.form['bio']
+        try:
+            login_session['user'] =auth.create_user_with_email_and_password(email, password)
+            user = { 'email': email, 'name': name, 'username': username, "bio": bio}
+            UID = login_session['user']['localId']
+            db.child('Users').child(UID).set(user)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+            print(error)
+    return render_template("signup.html")
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    error = ""
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        try:
+            tweet_data = { "title": title, "text": text,}
+            db.child("Tweets").push(tweet_data)
+        except:
+            error = ""
     return render_template("add_tweet.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
